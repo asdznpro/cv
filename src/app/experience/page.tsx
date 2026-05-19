88a7d42a9bd3bda630559eaddcd63c2d37c5d137
Andrew S. <asdznpro@gmail.com>
2026-05-19 08:48:12 +0700

    Update project configuration and layout details
    - Adjusted indentation in tsconfig.json for consistency.
    - Added font imports in globals.css.
    - Updated metadata in layout.tsx to reflect new project title and description.
    - Cleaned up Home component in page.tsx by removing unused elements.

---
+++ src/app/experience/page.tsx
@@ -0,0 +1,7 @@
+export default function Experience() {
+	return (
+		<div className='flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
+			EXPERIENCE
+		</div>
+	)
+}
---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,7 @@
+export default function Portfolio() {
+	return (
+		<div className='flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
+			PORTFOLIO
+		</div>
+	)
+}
